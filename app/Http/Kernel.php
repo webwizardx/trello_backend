51b80355cdd8ef5b0fcbab55659dedb2fceb7adc
Jonathan Alvarado <wizardx1407@gmail.com>
2022-10-19 17:12:50 -0400

    Add Locale middleware to handle translation
    * Minor changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,7 +42,8 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ResponseMetadata::class
+            \App\Http\Middleware\ResponseMetadata::class,
+            \App\Http\Middleware\Locale::class,
         ],
     ];
 
